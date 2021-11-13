3278ee3c7fdf3cc4af0a5f74d51a022dd29b1926
SergeyChalkov <seregilr@gmail.com>
2021-11-13 23:26:04 +0700

    feat: add flake8

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,15 +1,15 @@
- #!/usr/bin/env python
+#!/usr/bin/env python
 from brain_games.cli import welcome_user
- 
+
 
 def print_title():
-    print("Welcome to the Brain Games!")
+    print('Welcome to the Brain Games!')
 
 
 def main():
     print_title()
     user_name = welcome_user()
-    print(f"Hello, {user_name}!")
+    print(f'Hello, {user_name}!')
 
 
 if __name__ == '__main__':
