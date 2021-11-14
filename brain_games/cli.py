d3c7c3711019b6b81d5e511c9e82123d63a11463
SergeyChalkov <seregilr@gmail.com>
2021-11-14 17:18:08 +0700

    fix: github action code

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -3,13 +3,3 @@ import prompt
 
 def welcome_user():
     return prompt.string("May I have your name? ")
-
-
-
-def testing_actions():
-    """
-    this function is purely for test linter    """
-    return 2 + 3
-
-
-
