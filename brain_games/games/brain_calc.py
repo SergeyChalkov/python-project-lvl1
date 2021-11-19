99b5e42b6409fcab715a2a01c972c2b7b8abd46b
SergeyChalkov <seregilr@gmail.com>
2021-11-20 00:13:07 +0700

    Create brain-calc game

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -1,14 +1,35 @@
 from brain_games.engine import start_quiz_game
-from random import sample
+from random import choice, randint
+
+
+def create_expression():
+    number_one, number_two = randint(1, 100), randint(1, 100)
+    operator = choice('-+*')
+    expression = number_one, operator, number_two
+    answer = calculate_result(*expression)
+    return ' '.join(str(element) for element in expression), answer
+
+
+def calculate_result(number_one, operator, number_two):
+    if operator == '-':
+        return str(number_one - number_two)
+    elif operator == '+':
+        return str(number_one + number_two)
+    else:
+        return str(number_one * number_two)
+
+
+def get_quiz_questions():
+    return tuple((create_expression() for _ in range(number_of_rounds)))
 
 
 def main():
     start_quiz_game(quiz_rules, quiz_questions)
 
 
-
+number_of_rounds = 3
 quiz_rules = "What is the result of the expression?"
-quiz_questions = 1
+quiz_questions = get_quiz_questions()
 
 if __name__ == '__main__':
     main()
