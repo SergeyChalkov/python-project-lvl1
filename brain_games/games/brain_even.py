cf81b960815240ff2be7d0d310106f1bdf109ce1
SergeyChalkov <seregilr@gmail.com>
2021-11-20 00:12:25 +0700

    Add number of rounds to game

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -4,7 +4,7 @@ from random import sample
 
 def get_numbers_for_game():
     return ((number, is_even(number))
-            for number in sample(range(1, 10000), k=3))
+            for number in sample(range(1, 10000), k=number_of_rounds))
 
 
 def is_even(number):
@@ -15,6 +15,7 @@ def main():
     start_quiz_game(quiz_rules, quiz_questions)
 
 
+number_of_rounds = 3
 quiz_rules = "Answer 'yes' if the number is even, otherwise answer 'no'."
 quiz_questions = get_numbers_for_game()
 
