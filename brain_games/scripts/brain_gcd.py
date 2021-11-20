8bd1a2cea23fc4210a381dc8b3c0474435c60ab6
SergeyChalkov <seregilr@gmail.com>
2021-11-20 18:36:51 +0700

    Add get common divisor game

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from brain_games.games.quiz_engine import start_quiz_game
+from brain_games.games.game_gcd import get_quiz_questions
+
+
+def main():
+    start_quiz_game(quiz_rules, quiz_questions)
+
+
+quiz_rules = "Find the greatest common divisor of given numbers."
+quiz_questions = get_quiz_questions()
+
+if __name__ == '__main__':
+    main()
