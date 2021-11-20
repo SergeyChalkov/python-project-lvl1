20941e36fac95075f2fc488e9b69014b7d93e145
SergeyChalkov <seregilr@gmail.com>
2021-11-20 14:28:13 +0700

    Fix bug in calc game

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from brain_games.games.quiz_engine import start_quiz_game
-from brain_games.games.game_even import get_quiz_questions
+from brain_games.games.game_calc import get_quiz_questions
 
 
 def main():
