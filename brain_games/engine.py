0eb70dc05a386c5ae100d2c4b26a601396f7359a
SergeyChalkov <seregilr@gmail.com>
2021-11-26 22:03:58 +0700

    Remove not used html code Readmeclear

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -15,7 +15,9 @@ def start_game(quiz_rules, quiz_game):
         if user_input == answer:
             print("Correct!")
         else:
-            print(f"{user_input!r} is wrong answer ;(. Correct answer was {answer!r}.")
+            print(
+                f"{user_input!r} is wrong answer ;(. Correct answer was {answer!r}."
+            )
             print(f"Let's try again, {user_name}!")
             quit()
     else:
