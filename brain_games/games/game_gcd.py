b46725eb0b17aaeeae89f7f7ad6572f7454f5aee
SergeyChalkov <seregilr@gmail.com>
2021-11-30 21:50:06 +0700

    Fix _get_gcd function

--- brain_games/games/game_gcd.py
+++ brain_games/games/game_gcd.py
@@ -5,7 +5,15 @@ import random
 GAME_RULES = "Find the greatest common divisor of given numbers."
 
 
+def _get_gcd(lesser, greater):
+    if lesser == 0:
+        return greater
+    return _get_gcd(greater % lesser, lesser)
+
+
 def get_question():
     number_one, number_two = random.randint(1, 1000), random.randint(1, 1000)
-    answer = math.gcd(number_one, number_two)
+    if number_one > number_two:
+        number_one, number_two = number_two, number_one
+    answer = _get_gcd(number_one, number_two)
     return f"{number_one} {number_two}", str(answer)
